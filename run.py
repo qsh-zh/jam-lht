30565e042fc6cbb082f860a317823a7ab48677fb
zqsh <zqsh19970218@gmail.com>
2021-10-13 20:25:02 -0400

    feat : adding jammy and hyd_link before trainning

--- run.py
+++ run.py
@@ -7,6 +7,8 @@ def main(config: DictConfig):
 
     # Imports should be nested inside @hydra.main to optimize tab completion
     # Read more here: https://github.com/facebookresearch/hydra/issues/934
+    from jammy import link_hyd_run
+
     from src.train import train
     from src.utils import utils
 
@@ -20,6 +22,9 @@ def main(config: DictConfig):
     # Pretty print config using Rich library
     if config.get("print_config"):
         utils.print_config(config, resolve=True)
+
+    # quick link
+    link_hyd_run()
 
     # Train model
     return train(config)
